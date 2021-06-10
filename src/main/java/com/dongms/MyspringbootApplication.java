d63acbc32ed64d1d918ca05465ad92a8b2ad1eae
dongms <dongmingsheng20@163.com>
2021-06-10 14:17:47 +0800

    集成mybatis

--- src/main/java/com/dongms/MyspringbootApplication.java
+++ src/main/java/com/dongms/MyspringbootApplication.java
@@ -1,13 +1,16 @@
-package com.dongms.myspringboot;
+package com.dongms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.dongms.myspringboot.mapper"})
 public class MyspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringbootApplication.class, args);
+		System.out.println("启动成功！！！！");
 	}
 
 }
